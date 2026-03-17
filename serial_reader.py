b1d8c390786704042eb8f0e055acc6156ab1dc3a
Kevin Jiang <Jiangkevin10@gmail.com>
2026-03-17 02:26:58 -0400

    Added Rpm changes and change arudino code

--- serial_reader.py
+++ serial_reader.py
@@ -1,31 +1,40 @@
 import serial
 import time
 
-PORT = "/dev/ttyACM0"
-BAUD = 9600
-
-ser = serial.Serial(PORT, BAUD, timeout=1)
+ser = serial.Serial("/dev/ttyACM0", 9600, timeout=0.1)
 time.sleep(2)
 
-last_count = 0
+magnets_per_rev = 1
 
-print("Listening for counts...")
+count = 0
+last_rpm_count = 0
+last_rpm_time = time.time()
+
+print("Listening... Press Ctrl+C to stop.\n")
 
 try:
     while True:
         line = ser.readline().decode("utf-8", errors="ignore").strip()
+        now = time.time()
+        clock = time.strftime("%H:%M:%S", time.localtime(now))
 
-        if not line:
-            continue
+        if line and line.startswith("COUNT:"):
+            count = int(line.split(":")[1])
+            
 
-        print("RAW:", line)
+        if now - last_rpm_time >= 1.0:
+            delta_count = count - last_rpm_count
+            delta_time = now - last_rpm_time
 
-        if line.startswith("COUNT:"):
-            count = int(line.split(":")[1].strip())
+            if delta_time > 0:
+                rpm = ((delta_count / magnets_per_rev) / delta_time) * 60
+            else:
+                rpm = 0
 
-            if count != last_count:
-                print("Parsed count =", count)
-                last_count = count
+            print(f"[{clock}] RPM = {rpm:.2f}")
+
+            last_rpm_count = count
+            last_rpm_time = now
 
 except KeyboardInterrupt:
     print("\nStopped.")
